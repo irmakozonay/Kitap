0a467c4031fa111a769d63d5135821ba284be3f6
irmakozonay <irmakoz@msn.com>
2021-03-17 17:49:14 +0300

    delegate

--- Example/Kitap/ViewController.swift
+++ Example/Kitap/ViewController.swift
@@ -9,25 +9,42 @@
 import UIKit
 import Kitap
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ARTLabsDelegate {
 
+    @IBOutlet weak var startARButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let log = Logger()
         log.printLog()
         ARTLabs.setApiKey("M3QGD6HHBNLRDZQMBP")
+        ARTLabs.artlabs().delegate = self
+//        ARTLabs.artlabs().setActiveView(self.view)
     }
     
     @IBAction func startAR(_ sender: UIButton){
-        sender.isEnabled = false
+//        sender.isEnabled = false
         ARTLabs.artlabs().startARSession(productSKU: "")
     }
     
     @IBAction func showAR(){
-//        ARTLabs.artlabs().getModelURL(productSKU: "puma", completion: {(modelUrl) in
-//            print("modelUrl: \(modelUrl)")
-//        })
-        ARTLabs.artlabs().presentAR()
+        ARTLabs.artlabs().getModelURL(productSKU: "puma", completion: {(modelUrl) in
+            print("modelUrl: \(modelUrl)")
+        })
+//        ARTLabs.artlabs().presentAR()
+    }
+    
+    func modelLoadStarted() {
+        print("load started")
+        startARButton.isEnabled = false
+        activityIndicator.startAnimating()
+    }
+    
+    func modelLoadFinished() {
+        print("load finished")
+        startARButton.isEnabled = true
+        activityIndicator.stopAnimating()
     }
 
     override func didReceiveMemoryWarning() {
--- Kitap/Classes/ARTLabs.swift
+++ Kitap/Classes/ARTLabs.swift
@@ -9,12 +9,20 @@ import Foundation
 import QuickLook
 import ARKit
 
-public class ARTLabs : NSObject, QLPreviewControllerDataSource, QLPreviewControllerDelegate {
+public protocol ARTLabsDelegate {
+    func modelLoadStarted()
+    func modelLoadFinished()
+}
+
+public class ARTLabs : NSObject, QLPreviewControllerDataSource/*, QLPreviewControllerDelegate*/ {
     
     static let shared = ARTLabs()
-    var viewController = LoadingViewController()
+//    var viewController = LoadingViewController() //mail01 bu ve bunu kullanan ornekler
+//    var activeView: UIView?
     
     var modelURL: URL?
+    public var delegate : ARTLabsDelegate?
+    
     static var apiKey = ""
     
     override init(){}
@@ -28,13 +36,23 @@ public class ARTLabs : NSObject, QLPreviewControllerDataSource, QLPreviewControl
     }
     
     public func startARSession(productSKU: String){
-//        UIApplication.shared.keyWindow?.rootViewController?.present(viewController, animated: true, completion: nil)
+//        UIApplication.shared.keyWindow?.rootViewController?.present(viewController, animated: true, completion: nil) //mail01
+//        if let activeView = self.activeView {
+//            let width: CGFloat = 20;
+//            let activityIndicator = UIActivityIndicatorView(frame: CGRect(x: activeView.frame.size.width/2 - width/2, y: activeView.frame.size.height/2 - width/2, width: width, height: width))
+//            activityIndicator.startAnimating()
+//            self.activeView?.addSubview(activityIndicator)
+//        }
         downloadSampleUSDZ()
     }
     
+//    public func setActiveView(_ activeView: UIView) { //mail01 bunda sorun kisinin viewi uzerinde islem yapiyoruz ve view uzerinde yapilacak farkli islemleri kontrol edemeyiz + tasarim uyumsuz kalabilir
+//        self.activeView = activeView
+//    }
+    
     //mail3 ben olsam getModelURL + completion - getModelURL de urlden sonra download vb baya islem gerekiyo eklerdim
     //mail1 loading ekrani gerekli
-    //mail2 swift4 ile yapiyorum
+    //mail2 swift4 ile yapiyorum ve ios9 sonrasi destekli
     
     public func getModelURL(productSKU: String, completion: @escaping (_ modelUrl: String?) -> Void) { //mail2 or mail3 completion block ile olmali direk string return edemez - ya da semaphore kullanicam
         let url = URL(string: "https://staging.api.artlabs.ai/secure/product/" + productSKU)!
@@ -50,15 +68,16 @@ public class ARTLabs : NSObject, QLPreviewControllerDataSource, QLPreviewControl
                 print("No data")
                 return
             }
+            print(String(decoding: data, as: UTF8.self))
             if let httpResponse = response as? HTTPURLResponse {
                 if httpResponse.statusCode == 401 {
                     print("Please check your API key")
+                    return
                 } else if httpResponse.statusCode != 200 {
                     print("Response code: \(httpResponse.statusCode)")
+                    return
                 }
-                return
             }
-            print(String(decoding: data, as: UTF8.self))
             guard let product = try? JSONDecoder().decode(Product.self, from: data) else {
                 print("Error: Couldn't decode data")
                 return
@@ -73,6 +92,7 @@ public class ARTLabs : NSObject, QLPreviewControllerDataSource, QLPreviewControl
     
     func downloadSampleUSDZ() {
         print("start download")
+        delegate?.modelLoadStarted()
         let url = URL(string: "https://artlabs-3d.s3.eu-central-1.amazonaws.com/2a17bd30_2fe4_4202_81ad_b6cb3929e767_3e3ffd43d4.usdz")!
         let downloadTask = URLSession.shared.downloadTask(with: url) { [self] urlOrNil, responseOrNil, errorOrNil in
             guard let fileURL = urlOrNil else { return }
@@ -82,23 +102,14 @@ public class ARTLabs : NSObject, QLPreviewControllerDataSource, QLPreviewControl
                                             in: .userDomainMask,
                                             appropriateFor: nil,
                                             create: false)
-                print("fileURL: \(fileURL)");
-                print("url.lastPathComponent: \(url.lastPathComponent)");
+                print("fileURL: \(fileURL)")
                 let savedURL = documentsURL.appendingPathComponent("artlabs_3dmodel." + url.pathExtension/*url.lastPathComponent*/)
-                
-                //remove file
-                if FileManager.default.fileExists(atPath: savedURL.path) {
-                    do{
-                        try FileManager.default.removeItem(atPath: savedURL.path)
-                    } catch{
-                        print("Handle Exception")
-                    }
-                }
-                //
-                
+                removeFile(fileUrl: savedURL)
                 try FileManager.default.moveItem(at: fileURL, to: savedURL)
                 self.modelURL = savedURL
+                print("savedURL: \(savedURL)")
                 DispatchQueue.main.async {
+                    delegate?.modelLoadFinished()
                     self.presentAR()
                 }
             } catch {
@@ -108,12 +119,22 @@ public class ARTLabs : NSObject, QLPreviewControllerDataSource, QLPreviewControl
         downloadTask.resume()
     }
     
+    func removeFile(fileUrl: URL){
+        if FileManager.default.fileExists(atPath: fileUrl.path) {
+            do{
+                try FileManager.default.removeItem(atPath: fileUrl.path)
+            } catch{
+                print("Meshkraft - file manager cant removeitem")
+            }
+        }
+    }
+    
     public func presentAR(){
         guard modelURL != nil else { return }
         let previewController = QLPreviewController()
         previewController.dataSource = self
-        previewController.delegate = self
-//        viewController.dismiss(animated: false, completion: {UIApplication.shared.keyWindow?.rootViewController?.present(previewController, animated: true, completion: nil)})
+//        previewController.delegate = self
+//        viewController.dismiss(animated: true, completion: {UIApplication.shared.keyWindow?.rootViewController?.present(previewController, animated: true, completion: nil)})
         UIApplication.shared.keyWindow?.rootViewController?.present(previewController, animated: true, completion: nil)
 //        viewController.disable()
 //        viewController.present(previewController, animated: true, completion: nil)
@@ -126,7 +147,7 @@ public class ARTLabs : NSObject, QLPreviewControllerDataSource, QLPreviewControl
         return url as QLPreviewItem
     }
     
-    public func previewControllerWillDismiss(_ controller: QLPreviewController) {
-        viewController.dismiss(animated: false, completion: nil)
-    }
+//    public func previewControllerWillDismiss(_ controller: QLPreviewController) {
+//        viewController.dismiss(animated: true, completion: nil)
+//    }
 }
